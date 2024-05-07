d7c466a2136d115ec67b7d6070275343cc1a98b2
Your Name <you@example.com>
2024-05-07 11:01:42 +0530

    method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+class A {
+    String meth() {
+        return "Invoking method from class A";
+    }
+
+}
+class B extends A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+
+}
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+        B objb = new B();
+        System.out.println(objB.meth());
+    }
+     System.out.println(objB.meth());
+
+
+    }
 }
